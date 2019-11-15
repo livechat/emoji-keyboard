7406eb0a8f18e90761240cf6f27a87761d0bc43f
Michał Sypko <m.sypko@livechatinc.com>
2019-11-15 11:01:24 +0100

    loading improvents

--- js/client.js
+++ js/client.js
@@ -1,3 +1,4 @@
+const emojis = require('./emojis');
 const LiveChat = require('@livechat/agent-app-widget-sdk'),
     $ = require('jquery-browserify'),
     ifemoji = require('if-emoji'),
@@ -5,92 +6,110 @@ const LiveChat = require('@livechat/agent-app-widget-sdk'),
 
 window.EmojiKeyboard = {
 
+    emojiDOM: $('.emoji-set').clone(),
+
     getOS: function(ua) {
         return new parser(ua).getOS().name.toLowerCase().replace(/\W/, '');
     },
 
-    printEmojiCategory: function(emojisObject, category) {
-        let elementID = category.toLowerCase(),
-            anyEmojiSupported = 0;
-        $('.emoji-set').append('<p class="emoji-category" id="' + elementID + '"><span class="emoji-header">' + category + '</span></p>');
-        $.each(emojisObject[category], function () {
-            let unified = this.unified;
-            unified = unified.split('-');
-            let dec = [];
-            $.each(unified, function () {
-                dec.push(parseInt(this, 16));
+    preapreEmojiKeyboard: function() {
+        const that = this;
+        $.each(emojis, function (category) {
+            let elementID = category.toLowerCase(),
+                anyEmojiSupported = 0;
+            const emojiCategoryDOM = $('<p>', {
+                class: 'emoji-category',
+                id: elementID,
             });
-            let emoji = String.fromCodePoint.apply(this, dec);
-            if (ifemoji(emoji)) {
-                $('#' + elementID).append('<span class="emoji-single" data-emoji-code="' + emoji + '">' + emoji + '</span>');
-                anyEmojiSupported++;
-            }
+
+            $.each(this, function () {
+                let unified = this.unified;
+                unified = unified.split('-');
+                let dec = [];
+                $.each(unified, function () {
+                    dec.push(parseInt(this, 16));
+                });
+                let emoji = String.fromCodePoint.apply(this, dec);
+                if (ifemoji(emoji)) {
+                    emojiCategoryDOM.append($('<span>', {
+                        class: 'emoji-single',
+                        'data-emoji-code': emoji,
+                        html: emoji
+                    })); // '<span class="emoji-single" data-emoji-code="' + emoji + '">' + emoji + '</span>');
+                    anyEmojiSupported++;
+                }
+            });
+
+            that.emojiDOM.append(emojiCategoryDOM);
+
+            if (anyEmojiSupported === 0)
+                $("#" + elementID).remove();
         });
-        if(anyEmojiSupported === 0)
-            $("#"+elementID).remove();
+
     },
 
     init: function () {
-        LiveChat.init({
-            authorization: false
-        });
-
-        $('.emoji-set').on( "click", ".emoji-single", function() {
-            LiveChat.putMessage($(this).data('emoji-code'));
-        });
-
-        $.fn.isInViewport = function() {
-            let elementTop = $(this).offset().top;
-            let elementBottom = elementTop + $(this).outerHeight();
-
-            let viewportTop = $(window).scrollTop();
-            let viewportBottom = viewportTop + $(window).height();
-
-            return elementBottom > viewportTop && elementTop < viewportBottom;
-        };
-
-        $(window).on('resize scroll', function() {
-            let stop = false;
-            $('.emoji-category').each(function() {
-                if (stop === false) {
-                    let id = $(this).attr('id').toLowerCase();
-                    $('.chat-plugin__menu__item').removeClass('chat-plugin__menu__item--active');
-                    if ($(this).isInViewport()) {
-                        $('li[data-target-id="'+id+'"]').addClass('chat-plugin__menu__item--active');
-                        stop = true;
-                    }
-                }
+        const that = this;
+        $(document).ready(function () {
+            LiveChat.init({
+                authorization: false
             });
-        });
 
-        $.each(emojis, function (index) {
-            EmojiKeyboard.printEmojiCategory(emojis, index);
-        });
+            $('.emoji-set').on( "click", ".emoji-single", function() {
+                LiveChat.putMessage($(this).data('emoji-code'));
+            });
 
-        if($('.emoji-category').length === 0) {
-            const OS = this.getOS(navigator.userAgent);
-            let link;
-            switch (OS) {
-                case 'windows':
-                    link = 'http://caniemoji.com/microsoft-windows/';
-                    break;
-                case 'macos':
-                    link = 'http://caniemoji.com/os-x/';
-                    break;
-                default:
-                    link = 'http://caniemoji.com/';
-                    break;
+            $.fn.isInViewport = function() {
+                let elementTop = $(this).offset().top;
+                let elementBottom = elementTop + $(this).outerHeight();
+
+                let viewportTop = $(window).scrollTop();
+                let viewportBottom = viewportTop + $(window).height();
+
+                return elementBottom > viewportTop && elementTop < viewportBottom;
+            };
+
+            $(window).on('resize scroll', function() {
+                let stop = false;
+                $('.emoji-category').each(function() {
+                    if (stop === false) {
+                        let id = $(this).attr('id').toLowerCase();
+                        $('.chat-plugin__menu__item').removeClass('chat-plugin__menu__item--active');
+                        if ($(this).isInViewport()) {
+                            $('li[data-target-id="'+id+'"]').addClass('chat-plugin__menu__item--active');
+                            stop = true;
+                        }
+                    }
+                });
+            });
+
+            that.preapreEmojiKeyboard();
+
+            if(that.emojiDOM.find('.emoji-category').length === 0) {
+                const OS = that.getOS(navigator.userAgent);
+                let link;
+                switch (OS) {
+                    case 'windows':
+                        link = 'http://caniemoji.com/microsoft-windows/';
+                        break;
+                    case 'macos':
+                        link = 'http://caniemoji.com/os-x/';
+                        break;
+                    default:
+                        link = 'http://caniemoji.com/';
+                        break;
+                }
+
+                $('#os-support').attr('href', link);
+                $('.emoji-unsupported').show();
+                $('.chat-plugin__menu').remove();
+            } else {
+                $('.emoji-set').replaceWith(that.emojiDOM);
+                $('.chat-plugin__menu, .emoji-set').show();
             }
 
-            $('#os-support').attr('href', link);
-            $('.emoji-unsupported').show();
-            $('.chat-plugin__menu').remove();
-        } else {
-            $('.chat-plugin__menu, .emoji-set').show();
-        }
-
-        $('.app__loading').hide();
-
+            $('#loader').hide();
+        });
     }
 };
 
